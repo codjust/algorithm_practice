2a54be54c34824f4e31e5156b26b7355776b86d0
huchangwei <1040385974@qq.com>
2016-12-05 12:01:50 +0800

    添加三位取中，优化快速排序

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,12 +1,37 @@
 /*************************************************************************
 	> File Name: quicksort.cpp
-	> Author: 
-	> Mail: 
+	> Author:
+	> Mail:
 	> Created Time: 2016年11月23日 星期三 10时54分05秒
  ************************************************************************/
 
-#include<iostream>
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
+
+
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int SelectMidValue(int *A, int low, int high)
+{
+    //三位取中
+    int mid = low + (low + high)/2;
+    if(A[mid] > A[high])
+        swap(&mid, &high);
+    else if(A[low] > A[high])
+        swap(&low, &high);
+    else if(A[mid]>A[low])
+        swap(&mid, &low);
+
+  //  cout<<A[mid]<<" "<<A[low]<<" "<<A[high]<<endl;
+    return A[low];
+}
 
 void quicksort(int *A, int s,int t)
 {
@@ -14,7 +39,10 @@ void quicksort(int *A, int s,int t)
 
     int L = s;
     int R = t;
-    int X = A[L];
+    int X = SelectMidValue(A, L, R);
+  //  cout<<"X->:"<<X<<endl;
+    swap(A[L],X);//返回的值和最左边的数交换
+  //  cout<<"A[L]->:"<<A[L]<<endl;
     while(L<R)
     {
         while(L<R&&A[R]>X)
@@ -40,9 +68,15 @@ void quicksort(int *A, int s,int t)
 
 int main()
 {
-    int a[5] = {2,4,1,7,3};
-    quicksort(a,0,4);
-    for(int i=0;i<5;i++)
+    // int x=2;
+    // int y=3;
+    // swap(&x, &y);
+    // cout<<x<<" "<<y<<endl;
+    int a[] = {2,4,1,7,3,0,5};
+
+   // cout<<"address:"<<a[0]<<" "<<&a[0]<<endl;
+    quicksort(a,0,6);
+    for(int i=0;i<7;i++)
         cout<<a[i]<<" ";
     cout<<endl;
     return 0;
